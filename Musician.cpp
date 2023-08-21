6dd10857ea1916d7ab61e817ce35e44ba8d74475
Tang Piseth <tangpiseth43@gmail.com>
2023-08-22 06:55:51 +0700

    adding musician cpp

---
+++ Musician.cpp
@@ -0,0 +1,9 @@
+#include "Musician.h"
+
+Musician::Musician(){}
+
+Musician::Musician(std::stirng instrument, int experience){}
+
+std::string Musician::get_instrument(){}
+
+int Musician::get_experience(){}
