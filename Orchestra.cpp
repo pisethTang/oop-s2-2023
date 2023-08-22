6a83d109a059fe27afe53ef0811705bfc3ac799e
Tang Piseth <tangpiseth43@gmail.com>
2023-08-22 18:18:47 +0700

    de

--- Orchestra.cpp
+++ Orchestra.cpp
@@ -18,7 +18,7 @@ Orchestra::Orchestra(int size) //user-defined
 Orchestra::Orchestra(int size, Musician* musicians) //user-defined
 { 
     this -> size = size;
-    this -> musicians = new Musician[size];
+    this -> musicians = musicians;
 }
 
 // int* Orchestra::get_current_number_of_members(){
