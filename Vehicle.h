c2be7874b5232f2685b2b325a2cec8559a8e060e
Tang Piseth <tangpiseth43@gmail.com>
2023-08-29 20:17:54 +0700

    created files

--- Vehicle.h
+++ Vehicle.h
@@ -1,6 +1,15 @@
 #ifndef VEHICLE_H
 #define VEHICLE_H
+#include <ctime>
 
+class Vehicle{
+    private:
+        int ID;
+        std::time_t timeOfEntry; // the time at which the vehicle entered the parking lot, and it should be intialized to the time when the object is created!
+    public:
+        
+
+};
 
 
 
