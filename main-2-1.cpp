abd185bfb7bf672d79a216895ffa1ed7f59afc27
Tang Piseth <tangpiseth43@gmail.com>
2023-09-05 07:57:30 +0700

    adding

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -5,17 +5,11 @@
 int main(){
     // Appliance* a1;
     // a1->set_volume(50)
-    Fridge* a1 = new Fridge(50,50);
-    a1 -> set_powerRating(100);
-    a1 -> set_volume(50);
-
-    std::cout << "---Testing Fridge's inheritance of Appliance class-----" << std::endl;
-    std::cout << "What is the fridge's power consumption?\n";
-    std::cout << a1 -> getPowerConsumption() << " W." <<std::endl;
-    std::cout << "What is the fridge's volume?\n";
-    std::cout << a1 -> get_volume() << " litres" <<std::endl;
-    a1 -> set_volume(243);
-    std::cout << "New volume: " << a1 -> get_volume() << " litres\n";
+    Fridge* fridge = new Fridge();
+    fridge -> set_volume(100);
+    std::cout << "Initial volume: " << fridge -> get_volume() << std::endl;
+    fridge -> set_volume(150);
+    std::cout << "New volume: " << fridge -> get_volume() << std::endl;
 
 
 
