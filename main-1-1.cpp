ce56d281d1679929ab29b871d1a9bfe2ea5ddf6c
Tang Piseth <tangpiseth43@gmail.com>
2023-09-06 11:16:06 +0700

    adding

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -6,10 +6,11 @@ using namespace std;
 int main(){
     AirCraft plane(300); // set the plane to 300 tons
     plane.set_numberOfFlights(2); 
-    cout << "Fuel: " << plane.get_fuel() << "%" << endl;
-    plane.refuel();
+    // cout << "Fuel: " << plane.get_fuel() << "%" << endl;
+    plane.set_fuel(12);
     cout << "Weight: "<< plane.get_weight() <<endl;
     cout << "Fuel: " << plane.get_fuel() << "%" << endl;
+    plane.refuel();
     cout << "Number of flights: " << plane.get_numberOfFlights() << endl;
     plane.fly(10, 20); // the plane has been flown once
     cout << "Number of flights after a flight: " << plane.get_numberOfFlights() << endl; 
