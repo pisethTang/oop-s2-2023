ad80a5466d7733140ae652fd38b5c38d84b268f8
Tang Piseth <tangpiseth43@gmail.com>
2023-09-08 09:42:33 +0700

    added games

---
+++ OOP_Games/oop_game2.cpp
@@ -0,0 +1,19 @@
+#include <SFML/Graphics.hpp>
+#include <string>
+using namespace sf;
+
+class OOPGame{
+    private:
+        sf::RenderWindow* win;
+    public:
+        OOPGame(){
+            win = new sf::
+        }
+
+        ~OOPGame(){};
+}
+
+int main(){
+
+    return 0;
+}
