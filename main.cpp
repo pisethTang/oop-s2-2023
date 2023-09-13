b934a3f9131e687e633bbfc4620a7a042ca9e7c0
Tang Piseth <tangpiseth43@gmail.com>
2023-09-13 11:43:19 +0700

    adding games

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+int main(){
+    std::cout << "Hello from SFML!" << std::endl;
+    std::cin.get();
+    return 0;
+}
