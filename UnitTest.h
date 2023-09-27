9afabc93b1ac5e03b71b6993c0ba21c049f5bd4b
Tang Piseth <tangpiseth43@gmail.com>
2023-09-27 13:44:54 +0700

    adding 3 test cases to UnitTest class

---
+++ UnitTest.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Addition.h"
+
+class UnitTest{
+    private:
+        void testAddition(){
+            Addition addition;
+            if(addition.add(1,2) != 3){
+                std::cout << "Test 1 failed!" <<std::endl;
+            }
+            // Add more tests here
+            else if(addition.add(100,200) != 300) std::cout << "Test 2 failed!" <<std::endl;
+            else if(addition.add(0, 9) != 9) std::cout << "Test 3 failed!" <<std::endl;
+            else if(addition.add(10.2, 10) != 20.2) std::cout << "Test 4 failed!" <<std::endl; 
+            else std::cout << "All tests passed!" <<std::endl;
+        }
+    public:
+        void runTests(){
+            testAddition();
+        }
+};
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "UnitTest.h"
+
+
+int main(){
+    UnitTest unitTest;
+    unitTest.runTests();
+    return 0;
+}
