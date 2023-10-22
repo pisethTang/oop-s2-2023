453ffbe9410bff1b691b09f1b2635da08cf1b9f8
Tang Piseth <tangpiseth43@gmail.com>
2023-10-22 10:35:49 +1030

    9th: Question 2: made explode() return an explosion target (Still confused with the wording of the questions)

--- Mine.h
+++ Mine.h
@@ -8,13 +8,13 @@ class Mine: public GameEntity{
     public:
         Mine(int x, int y): GameEntity(x,y, 'M'){ }
         Mine(): Mine(0,0){ }
-        void explode(){
+        Explosion explode(){
             // returns an Explosion object
             
             std::cout << "Mine exploded!" << std::endl;
             // change Mine type to 'X'
             
-            
+            return Explosion(2,2);
         }
 
 };
--- git_push_script.sh
+++ git_push_script.sh
@@ -4,7 +4,7 @@
 git add .
 
 # Commit with a specified message
-git commit -m "8th: Question 2 1st push"
+git commit -m "9th: Question 2: made explode() return an explosion target (Still confused with the wording of the questions)"
 
 # Push to the remote repository of week 12
 git push origin workshop12
