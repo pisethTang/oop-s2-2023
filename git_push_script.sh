d95a84a9f634e766f1df58c571bdcc121c2e1ad6
Tang Piseth <tangpiseth43@gmail.com>
2023-10-24 11:13:02 +1030

    Question 2

---
+++ git_push_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Add all changes
+git add .
+
+# Commit with a specified message
+git commit -m "Question 2"
+
+# Push to the remote repository of week 12
+git push origin workshop12
--- main-2.cpp
+++ main-2.cpp
@@ -5,21 +5,22 @@
 
 
 int main(){
-    Mine* mine = new Mine;
-    Ship* ship = new Ship;
+    // Mine* mine = new Mine;
+    // Ship* ship = new Ship;
 
-    std::cout << "Ship's type: " << ship -> getType() << std::endl;
-    std::cout << "Mine's type: " << mine -> getType() << std::endl;
+    // std::cout << "Ship's type: " << ship -> getType() << std::endl;
+    // std::cout << "Mine's type: " << mine -> getType() << std::endl;
 
-    Explosion* explosion = mine -> explode();
-    explosion -> apply(ship);
+    // // after the mine is exploded, we need to apply the explosion to the ship
+    // Explosion* explosion = mine -> explode();
+    // explosion -> apply(ship);
 
-    std::cout << "Ship's type: " << ship -> getType() << std::endl;
-    std::cout << "Mine's type: " << mine -> getType() << std::endl;    
+    // std::cout << "Ship's type: " << ship -> getType() << std::endl;
+    // std::cout << "Mine's type: " << mine -> getType() << std::endl;    
 
-    delete mine;
-    delete ship;
-    delete explosion;
+    // delete mine;
+    // delete ship;
+    // delete explosion;
 
     return 0;
 }
