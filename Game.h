7d93fc4422dc71e2cbbe45abaf5d7748ed42eaca
Tang Piseth <tangpiseth43@gmail.com>
2023-10-25 14:13:26 +1030

    commit message 1

--- Game.h
+++ Game.h
@@ -13,7 +13,7 @@ class Game{
         int gridWidth;
         int gridHeight;
     public:
-        std::vector<Cell*> & getGrid(){return grid;}
+        std::vector<Cell*> getGrid(){return grid;}
         void initGame(int numCharacters, int numTraps, int gridWidth, int gridHeight){
             for(int i=0;i<numCharacters; i++){
                 std::tuple<int , int> randomPos = Utils::generateRandomPos( gridWidth, gridHeight);
@@ -39,7 +39,9 @@ class Game{
                 if(grid[i] -> getType() == 'C'){
                     Character* character = dynamic_cast<Character*>(grid[i]);
                     character -> move(1,0); // move the chracter 1 distance in the x-axis
+                    if(character == nullptr) std::cout << "character is nullptr\n";
                 }
+                std::cout << "first loop ok!\n";
 
                 for(auto cell: grid){
                     if(cell -> getType() == 'C'){
@@ -47,26 +49,26 @@ class Game{
                         // if(character == nullptr) std::cout << "character is null" << std::endl;
                         // else std::cout << "character is not null\n";
                         std::tuple<int, int> ch_pos = character -> getPos();
-                        bool b1 = std::get<0>(ch_pos) > 0 && std::get<0>(ch_pos) < gridWidth;
-                        bool b2 = std::get<1>(ch_pos) > 0 && std::get<1>(ch_pos) < gridHeight;
-                        if(b1 && b2 == true) {
+                        bool b1 = std::get<0>(ch_pos) < 0 && std::get<0>(ch_pos) > gridWidth;
+                        bool b2 = std::get<1>(ch_pos) < 0 && std::get<1>(ch_pos) > gridHeight;
+                        if(b1 && b2 == true) { // check if any character has stepped outside of the grid
                             std::cout << "Character has won the game!" << std::endl;
                             return;
                         }
-                        else{
+                         // if not then 
                         for(auto cell1: grid){
                             if(cell1 -> getType() == 'T'){
                                 Trap* trap = dynamic_cast<Trap*>(cell1);
-                                if(trap == nullptr) return;
+                                if(trap == nullptr) std::cout << "trap is null";
                                 //std::cout << "trap is null" << std::endl;
                                 // else std::cout << "trap is not null\n";
-                                if(Utils::calculateDistance(character -> getPos(), trap -> getPos()) <= trapActivationDistance){
+                                if(Utils::calculateDistance(character -> getPos(), trap -> getPos()) < trapActivationDistance){
                                     trap -> apply(*character);
                                 }
 
                             }
                         }
-                        }
+                        
                     }
                 }
 
